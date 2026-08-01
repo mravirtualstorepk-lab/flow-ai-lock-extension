a583a50d94cacbffcc21e75419e2252032fda532
mravirtualstorepk-lab <mravirtualstorepk@gmail.com>
2026-08-01 09:22:49 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,28 +1,31 @@
-const USERNAME = "admin";
-const PASSWORD = "123456";
-
 document.getElementById("login").addEventListener("click", async () => {
 
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if (username === USERNAME && password === PASSWORD) {
+    const data = await chrome.storage.local.get("users");
+    const users = data.users || [];
 
-        await chrome.storage.local.set({
-            loggedIn: true
-        });
+    const user = users.find(u =>
+        u.username === username &&
+        u.password === password
+    );
 
-        chrome.tabs.create({
-            url: "https://labs.google/fx/tools/flow"
-        });
-
-        window.close();
-
-    } else {
-
+    if (!user) {
         document.getElementById("msg").textContent =
             "Invalid Username or Password";
-
+        return;
     }
 
+    await chrome.storage.local.set({
+        loggedIn: true,
+        currentUser: user,
+        loginTime: Date.now()
+    });
+
+    chrome.tabs.create({
+        url: "https://labs.google/fx/tools/flow"
+    });
+
+    window.close();
 });
